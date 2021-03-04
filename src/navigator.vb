84298d105c9810cfb01c4d82d4f53f3a574249f2
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-03-04 16:37:37 +0100

    prepare for new commit

--- src/navigator.vb
+++
@@ -1,2 +0,0 @@
-﻿Public Class navigator
-End Class
--- src/validkey.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class validkey
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Form1.Close()
-    End Sub
-End Class
